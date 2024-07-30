afbd51d67a291f1986c7f574927cf29aedb4e1e3
cristidana <47077247+cristidana@users.noreply.github.com>
2024-07-30 08:43:05 +0300

    Last

--- setup_db.sql
+++ setup_db.sql
@@ -1,23 +1,24 @@
-CREATE DATABASE IF NOT EXISTS chat_app;
-
+DROP DATABASE IF EXISTS chat_app;
+CREATE DATABASE chat_app;
 USE chat_app;
 
-CREATE TABLE IF NOT EXISTS users (
-                                     id INT AUTO_INCREMENT PRIMARY KEY,
-                                     username VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    role ENUM('USER_ROLE', 'ADMIN_ROLE') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(50) NOT NULL UNIQUE,
+                       password_hash VARCHAR(255) NOT NULL,
+                       role ENUM('USER_ROLE', 'ADMIN_ROLE') NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS messages (
-                                        id INT AUTO_INCREMENT PRIMARY KEY,
-                                        user_id INT NOT NULL,
-                                        message TEXT NOT NULL,
-                                        sender ENUM('user', 'admin') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-    );
+CREATE TABLE messages (
+                          id INT AUTO_INCREMENT PRIMARY KEY,
+                          user_id INT NOT NULL,
+                          message TEXT NOT NULL,
+                          sender ENUM('user', 'admin') NOT NULL,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
--- Insert default admin user
-INSERT INTO users (username, password_hash, role) VALUES ('admin', '$2a$12$3CjJmVxPhEE4DZnvhEJNKu0AwwzmBsUvyZoCCDELabUaUPyACsxdi', 'ADMIN_ROLE');
+-- Check if the 'admin' user already exists before inserting
+INSERT INTO users (username, password_hash, role)
+SELECT 'admin', '$2a$12$3CjJmVxPhEE4DZnvhEJNKu0AwwzmBsUvyZoCCDELabUaUPyACsxdi', 'ADMIN_ROLE'
+    WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin');
